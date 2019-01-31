159010cfb9a241f780b2cccc55781b0b2ca356c8
Nikitosina1981 <nikita.krutov@gmail.com>
2019-01-31 21:58:22 +0500

    commit with test update

--- src/main/java/CallBookApp.java
+++ src/main/java/CallBookApp.java
@@ -5,7 +5,8 @@ public class CallBookApp
     public static void main(String[] args)
     {
         CallBook callBook = new CallBook();
-        if (callBook.requestPhones()!=null) System.out.println(callBook.getPhones());
+        final int counter=1;
+        if (callBook.requestPhones().size()>0) callBook.getPhones().forEach(System.out::println);
         else System.out.println("Неверное ФИО или ошика ввода...");
     }
 }
